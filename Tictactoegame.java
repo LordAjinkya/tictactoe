787d038f62318278b04b03f2a0283cd1ba055274
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-05 14:26:48 +0530

    Added: UC2

--- Tictactoegame.java
+++ Tictactoegame.java
@@ -1,9 +1,37 @@
+import java.util.*;
 package tictactoe;
 
+import java.awt.Choice;
+
 public class Tictactoegame {
+	String[] board;
+	String chance;
+	static String game() {
+		 char[] board =new char[10];
+		
+		for (int i=0;i<10;i++) {
+			String line=null;
+			
+		}
+		
+			
+		}
+	class chooseuser{
+		chooseuser(){
+			Choice c=new Choice();
+			c.add("X");
+			c.add("O");
+			
+		}
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to tictactoe game");
+		game();
+		System.out.println("Choose your letter");
+		new chooseuser();
 
 	}
 
