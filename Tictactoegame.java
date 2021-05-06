4cb6578212177a997cf2e00c6badb8ea5b4ee10c
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-06 20:37:28 +0530

    Added: code to check free position in box

--- Tictactoegame.java
+++ Tictactoegame.java
@@ -22,27 +22,34 @@ public class Tictactoegame {
 	static void printBoard()
     {
         System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                           + board[1] + " | " + board[2]
-                           + " |");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2]  + " |");
+               
+                           
         System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                           + board[4] + " | " + board[5]
-                           + " |");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5]  + " |");
+               
+                           
         System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                           + board[7] + " | " + board[8]
-                           + " |");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8]  + " |");
+               
+                           
         System.out.println("|---|---|---|");
     }
+	
+	//To check if the player is free to move
+	public boolean Position(int location) {
+		if (board[location] == 'X' || board[location] == 'O') { 
+			System.out.println("choose other position");
+			return true;
+		} else {
+			return false;
+		}
+	}
 		
 	     
 	    
 	    	
-	    
-	    
-	
-	
+	    	
 }
 
 
