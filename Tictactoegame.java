d8d35abb8456258a27acb958725e439ef2c9ccf5
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-07 11:14:47 +0530

    Added: code to check the winner

--- Tictactoegame.java
+++ Tictactoegame.java
@@ -2,7 +2,7 @@
 package tictactoe;
 import java.util.Scanner;
 import java.util.Random;
-
+import java.util.*;
 
 public class Tictactoegame {
 	
@@ -49,6 +49,8 @@ public class Tictactoegame {
 		}
 	}
 	
+	 
+	
 	public void letterchoosing() {
 		Scanner scan = new Scanner(System.in);
 		char player = scan.next().charAt(0); // getting input from player
@@ -72,6 +74,59 @@ public class Tictactoegame {
 	         System.out.println("Computer is playing");
 	      }
 	   }
+	//check for winner
+	static String turn;
+	
+	static String checkWinner() {
+		for (int a = 0; a < 8; a++) 
+		{
+			String.valueOf int
+			String line = null;
+			switch (a) {
+			case 0:
+				line = board[0] + board[1] + board[2];
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5];
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6];
+				break;
+			}
+			if (line.equals("XXX")) {
+				return "X";
+			} else if (line.equals("OOO")) {
+				return "O";
+			}
+		}
+
+		for (int a = 0; a < 9; a++) {
+			if (Arrays.asList(board).contains(String.valueOf(a+1))) {
+				break;
+			}
+			else if (a == 8) return "draw";
+		}
+
+		System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+		return null;
+	}
+	
+          
 	
 	//main method
 	public static void main(String[] args) {
@@ -82,7 +137,13 @@ public class Tictactoegame {
 		boardgame.Position(0);
 		boardgame.letterchoosing();
 		boardgame.Toss();
-	}
+		
+		}
+	
+		
+		
+		
+	
 		
 		
 			    		
